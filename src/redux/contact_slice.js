41c1e0f8e1c37859e9c079e976536b599e257521
Dima Savkov <dimsaw85@gmail.com>
2022-07-23 11:37:00 +0300

    ready select redux

--- src/redux/contact_slice.js
+++ src/redux/contact_slice.js
@@ -1,25 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { contactDefault } from '../data/data';
-import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/types';
 
 const initialState = {
   items: contactDefault,
   filter: '',
 };
 
-const contactSlice = createSlice({
+const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
 
-    reducers: {
-        addContact(state, { payload }) {
-            state.items.push(payload)
-        }
-        deleteContact(state, { payload }) {
-            state.items = state.items.filter(({id}) => id !== payload)
-        }
-        filterContact(state, { payload }) {
-            state.filter = payload
-        }
+  reducers: {
+    addContact(state, { payload }) {
+      state.items.push(payload);
+    },
+    deleteContact(state, { payload }) {
+      state.items = state.items.filter(({ id }) => id !== payload);
+    },
+    filterContact(state, { payload }) {
+      state.filter = payload;
+    },
   },
 });
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+export const persisteContactReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
+export const { addContact, deleteContact, filterContact } =
+  contactsSlice.actions;
+export default contactsSlice.reducer;
