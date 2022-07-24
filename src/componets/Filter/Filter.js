591440a89468c7b96ff5c9f5246ca7f4e30cf031
Dima Savkov <dimsaw85@gmail.com>
2022-07-24 17:29:41 +0300

    finish

--- src/componets/Filter/Filter.js
+++ src/componets/Filter/Filter.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import s from './Filter.module.css';
 
-const Filter = ({ value, onChange }) => (
-  <label className={s.label}>
-    <span className={s.text}>Find contacts by name</span>
-    <input className={s.input} type="text" value={value} onChange={onChange} />
-  </label>
-);
+import { useDispatch } from 'react-redux';
+import { filterContact } from '../../redux/contact_slice';
+
+const Filter = () => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <label className={s.label}>
+        <span className={s.text}>Find contacts by name</span>
+        <input
+          className={s.input}
+          type="text"
+          onChange={e => dispatch(filterContact(e.target.value.trim()))}
+        />
+      </label>
+    </>
+  );
+};
 
 export default Filter;
-
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
