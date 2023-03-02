93e1ba390b92b1504cf6452c39fa8d19fefb0ba1
Dima Savkov <dimsaw85@gmail.com>
2023-03-02 10:29:41 +0200

    change index.html

--- src/App.js
+++ src/App.js
@@ -1,62 +1,10 @@
-// import React, { useState } from 'react';
-
 import ContactsList from './componets/ContactsList';
 import ContactForm from './componets/ContactForm';
 import Filter from './componets/Filter';
 import Container from './componets/Container';
-// import useLocalStorage from './hooks/useLocalStorage';
+
 import s from './App.module.css';
-// import shortid from 'shortid';
 
-// import Notiflix from 'notiflix';
-// Notiflix.Notify.init({
-//   width: '380px',
-//   position: 'center-top',
-//   failure: {
-//     background: '#00bfff',
-//     textColor: '#fff',
-//   },
-// });
-
-// export default function App() {
-//   const [contacts, setContacts] = useLocalStorage('contacts');
-//   const [filter, setFilter] = useState('');
-
-//   const formSubmitHandler = ({ name, number }) => {
-//     const contact = {
-//       id: shortid.generate(),
-//       name,
-//       number,
-//     };
-
-//     if (
-//       contacts.find(
-//         contact => contact.name.toLowerCase() === name.toLowerCase(),
-//       )
-//     ) {
-//       return Notiflix.Notify.failure(`${name} is alredy in contacts`);
-//     }
-
-//     setContacts([contact, ...contacts]);
-//   };
-
-//   const changeFilter = e => {
-//     setFilter(e.currentTarget.value);
-//   };
-
-//   const getVisibleContacts = () => {
-//     const normolizedFilter = filter.toLowerCase();
-
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normolizedFilter),
-//     );
-//   };
-
-//   const deleteContact = id => {
-//     setContacts(contacts.filter(contact => contact.id !== id));
-//   };
-
-//   const filterContacts = getVisibleContacts();
 export default function App() {
   return (
     <>
